6edd82284931f80c80a096a844cf7c5595d8585f
Neelima <neelima.nalin@gmail.com>
2018-05-01 14:07:27 -0400

    new TC and update as setUpClass()

---
+++ TestSuite.py
@@ -0,0 +1,5 @@
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from  selenium.webdriver.common.by import By
+
--- UnitTest.py
+++ UnitTest.py
@@ -8,17 +8,30 @@ from selenium.webdriver import DesiredCapabilities
 # 2. Then you fill it with functions that start with ‘test_’.
 # 3. You run the tests by placing unittest.main() in your file, usually at the bottom
 # 4. Methods setUp() and teatDown() are called prior and after each test case
+#  5. setUpClass() and tearDownClass() does the same functionality but using single session/window
 
 class SearchText(unittest.TestCase):
-    def setUp(self):
-        # create session
+    @classmethod
+    def setUpClass(inst):
+        # create a new Firefox session
         cap = DesiredCapabilities.FIREFOX
         cap['marionette'] = False
-        self.driver = webdriver.Firefox(capabilities=cap)
-        self.driver.implicitly_wait(30)
-        self.driver.maximize_window()
-        # go to homepage
-        self.driver.get("http://www.google.com")
+        inst.driver = webdriver.Firefox(capabilities=cap)
+        inst.driver.implicitly_wait(30)
+        inst.driver.maximize_window()
+        # navigate to the application home page
+        inst.driver.get("http://www.google.com/")
+        inst.driver.title
+
+    # def setUp(self):
+    #     # create session
+    #     cap = DesiredCapabilities.FIREFOX
+    #     cap['marionette'] = False
+    #     self.driver = webdriver.Firefox(capabilities=cap)
+    #     self.driver.implicitly_wait(30)
+    #     self.driver.maximize_window()
+    #     # go to homepage
+    #     self.driver.get("http://www.google.com")
 
     def test_search_by_text(self):
         #search textbox
@@ -28,7 +41,7 @@ class SearchText(unittest.TestCase):
         self.search_field.submit()
         lists = self.driver.find_elements_by_class_name("r")
         no=len(lists)
-        self.assertEquals(11,len(lists))
+        self.assertEquals(11,len(lists),"LIMIT EXCEEDED!!!!")
         # no = len(lists)
         # self.assertEquals(10,no)
     def test_search_by_name(self):
@@ -38,8 +51,13 @@ class SearchText(unittest.TestCase):
         list_new = self.driver.find_elements_by_class_name("r")
         self.assertEqual(14, len(list_new))
 
-    def tearDown(self):
-        self.driver.quit()
+    @classmethod
+    def tearDownClass(inst):
+        # close the browser window
+        inst.driver.quit()
+
+    # def tearDown(self):
+    #     self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
