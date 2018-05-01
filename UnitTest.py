92e4e75c914e9bb22a98741b4734944f7b0cfa41
Neelima <neelima.nalin@gmail.com>
2018-05-01 13:16:12 -0400

    setup() and tearDown()

--- UnitTest.py
+++ UnitTest.py
@@ -1,10 +1,20 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+
+# UnitTest:
+# The standard workflow is:
+# 1. You define your own class derived from unittest.TestCase.
+# 2. Then you fill it with functions that start with ‘test_’.
+# 3. You run the tests by placing unittest.main() in your file, usually at the bottom
+# 4. Methods setUp() and teatDown() are called prior and after each test case
 
 class SearchText(unittest.TestCase):
     def setUp(self):
         # create session
-        self.driver = webdriver.Chrome()
+        cap = DesiredCapabilities.FIREFOX
+        cap['marionette'] = False
+        self.driver = webdriver.Firefox(capabilities=cap)
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
         # go to homepage
@@ -16,11 +26,20 @@ class SearchText(unittest.TestCase):
         self.search_field.clear()
         self.search_field.send_keys("selenium automation")
         self.search_field.submit()
-        lists = self.driver.find_element_by_xpath()
-        print(lists)
+        lists = self.driver.find_elements_by_class_name("r")
+        no=len(lists)
+        self.assertEquals(11,len(lists))
         # no = len(lists)
         # self.assertEquals(10,no)
+    def test_search_by_name(self):
+        self.search_field = self.driver.find_element_by_name("q")
+        self.search_field.send_keys("python class")
+        self.search_field.submit()
+        list_new = self.driver.find_elements_by_class_name("r")
+        self.assertEqual(14, len(list_new))
 
+    def tearDown(self):
+        self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
